19bdf470b52dabb490faa2280a93063d619f2494
Ayan Upadhaya <ayanu881@gmail.com>
2025-07-13 02:04:55 +0600

    configured for vercel deployment

---
+++ api/index.js
@@ -0,0 +1,6 @@
+const serverless = require("serverless-http");
+const createApp = require("../server/app");
+
+const app = createApp();
+module.exports = app;
+module.exports.handler = serverless(app);
--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,35 @@
 const mongoose = require("mongoose");
-mongoose.set("strictQuery", true);
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`Database Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.log(error);
-  }
-};
+
+
+
+mongoose.set("strictQuery", false); // or false, if you prefer
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+   if (!cached.promise) {
+     cached.promise = mongoose
+       .connect(process.env.MONGODB_URI, {
+         useNewUrlParser: true,
+         useUnifiedTopology: true,
+       })
+       .then((mongoose) => {
+         console.log("✅ MongoDB connected");
+         return mongoose;
+       })
+       .catch((err) => {
+         console.error("❌ MongoDB connection error:", err);
+         throw err;
+       });
+   }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
 module.exports = connectDB;
