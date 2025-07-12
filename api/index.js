5af626d359139b9561a87ca76b18f06d53bda799
Ayan Upadhaya <ayanu881@gmail.com>
2025-07-13 02:30:35 +0600

    structure changed

--- api/index.js
+++
@@ -1,6 +0,0 @@
-const serverless = require("serverless-http");
-const createApp = require("../server/app");
-
-const app = createApp();
-module.exports = app;
-module.exports.handler = serverless(app);
--- app.js
+++ app.js
@@ -1,7 +1,51 @@
-const createApp = require("./server/app");
-const app = createApp();
-const port = process.env.PORT || 3000;
+require("dotenv").config();
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const methodOverride = require("method-override");
+const connectDB = require("./server/config/db");
+const session = require("express-session");
+const passport = require("passport");
+const MongoStore = require("connect-mongo");
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+const app = express();
+
+app.use(
+  session({
+    secret: "note-ejs-app",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride("_method"));
+
+// Conntect to Database
+connectDB();
+
+// Static Files
+app.use(express.static("public"));
+
+// Templating Engine
+app.use(expressLayouts);
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+
+// Routes
+app.use("/", require("./server/routes/auth"));
+app.use("/", require("./server/routes/index"));
+app.use("/", require("./server/routes/dashboard"));
+
+// Handle 404
+app.get("*", function (req, res) {
+  res.status(404).render("404");
 });
+
+module.exports = app;
